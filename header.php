0d6cc398ef18ce771a44ff52b5c3f8e27c822696
hesslau <hesslau@gmail.com>
2015-09-29 13:42:34 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <main>
+ * and the left sidebar conditional
+ *
+ * @since 1.0.0
+ */
+?><!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if IE]><script src="<?php echo BAVOTASAN_THEME_URL; ?>/library/js/html5.js"></script><![endif]-->
+	<?php wp_head(); ?>
+</head>
+<?php
+$bavotasan_theme_options = bavotasan_theme_options();
+$space_class = '';
+?>
+<body <?php body_class(); ?>>
+
+	<div id="page">
+
+		<header id="header">
+			<nav id="site-navigation" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<h3 class="sr-only"><?php _e( 'Main menu', 'arcade' ); ?></h3>
+				<a class="sr-only" href="#primary" title="<?php esc_attr_e( 'Skip to content', 'arcade' ); ?>"><?php _e( 'Skip to content', 'arcade' ); ?></a>
+
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				    </button>
+				</div>
+
+				<div class="collapse navbar-collapse">
+					<?php
+					$menu_class = ( is_rtl() ) ? ' navbar-right' : '';
+					wp_nav_menu( array( 'theme_location' => 'primary', 'container' => '', 'menu_class' => 'nav navbar-nav' . $menu_class, 'fallback_cb' => 'bavotasan_default_menu' ) );
+					?>
+					<?php get_search_form(); ?>
+				</div>
+			</nav><!-- #site-navigation -->
+
+			 <div class="title-card-wrapper">
+                <div class="title-card">
+    				<div id="site-meta">
+    					<!--<h1 id="site-title">
+    						<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+    					</h1>-->
+    					<?php if ( $bavotasan_theme_options['header_icon'] ) { ?>
+    					<i id="homelogo" class="fa"></i>
+    					<?php } else {
+    						$space_class = ' class="margin-top"';
+    					} ?>
+
+							<?php if ( is_front_page() ): ?>
+	    					<h2 id="site-description"<?php echo $space_class; ?>>
+	    						<?php bloginfo( 'description' ); ?>
+	    					</h2>
+								<?php
+								/**
+								 * You can overwrite the defeault 'See More' text by defining the 'BAVOTASAN_SEE_MORE'
+								 * constant in your child theme's function.php file.
+								 */
+								if ( ! defined( 'BAVOTASAN_SEE_MORE' ) )
+									define( 'BAVOTASAN_SEE_MORE', __( 'See More', 'arcade' ) ); ?>
+
+								<br>
+								<a href="#" id="" class="btn btn-default btn-lg">Learn More</a>
+	    					<a href="#" id="" class="btn btn-default btn-lg">Book A Room</a>
+	    					<a href="#" id="" class="btn btn-default btn-lg">Become Volunteer</a>
+							<?php endif; ?>
+    				</div>
+
+    				<?php
+    				// Header image section
+    				bavotasan_header_images();
+    				?>
+				</div>
+			</div>
+
+		</header>
+
+		<main>
---
+++ page-templates/template-rooms.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template Name: Rooms
+ *
+ * Description: A full width page template that will display 4 posts in a block without any sidebars
+ *
+ * @since 1.0.3
+ */
+
+$args = array(
+	'post_type' => 'post',
+	'tax_query' => array(
+		array(
+			'taxonomy' => 'category',
+			'field'    => 'slug',
+			'terms'    => array("rooms")
+		),
+	),
+);
+$rooms_query = new WP_Query( $args );
+$force_excerpt = true;
+?>
+
+
+<?php if ( ! is_front_page() )	get_header(); ?>
+
+
+	<div class="container">
+		<div class="row">
+        	<div id="primary" <?php bavotasan_primary_attr(); ?>>
+        		<?php
+        		if ( $rooms_query->have_posts() ) :
+        			while ( $rooms_query->have_posts() ) : $rooms_query->the_post(); ?>
+
+							<!--- post start --->
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?>>
+								<?php
+								// Display a thumbnail if one exists and not on single post
+								bavotasan_display_post_thumbnail();
+
+									get_template_part( 'content', 'header' ); ?>
+
+									<div class="entry-content description clearfix">
+									<?php
+										// Show only excerpt of article
+										the_excerpt();
+									?>
+									</div><!-- .entry-content -->
+									<?php if ( is_singular() && ! is_front_page() )
+										get_template_part( 'content', 'footer' ); ?>
+							</article><!-- #post-<?php the_ID(); ?> -->
+							<!-- post end -->
+
+
+							<?php
+        			endwhile;
+
+        			bavotasan_pagination();
+        		else :
+        			get_template_part( 'content', 'none' );
+        		endif;
+        		?>
+        	</div>
+            <?php get_sidebar(); ?>
+		</div>
+	</div>
+
+<?php if ( ! is_front_page() ) get_footer(); ?>
